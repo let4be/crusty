baaad729b3a14b42c89ed39deb574689f6947675
Sergey F <let4be@gmail.com>
2021-06-06 13:03:45 +0300

    do not follow links with rel="no-follow"

--- main/src/main.rs
+++ main/src/main.rs
@@ -119,6 +119,10 @@ impl Crusty {
 		ddc: &mut TtlCache<String, ()>,
 		cfg: &CrustyConfig,
 	) -> Option<String> {
+		if lnk.rel.to_lowercase() == "no-follow" {
+			return None
+		}
+
 		let domain = lnk.host()?;
 
 		if domain.len() < 3 || !domain.contains('.') || domain == *task_domain || ddc.contains_key(&domain) {
