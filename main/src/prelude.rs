3f4d0ac5df7fd4fd72c0ac6312b8fac99606dade
Sergey F <let4be@gmail.com>
2021-06-20 00:19:34 +0300

    updated tracing-tools

--- main/src/prelude.rs
+++ main/src/prelude.rs
@@ -12,7 +12,7 @@ pub use anyhow::{anyhow, Context as _};
 pub use crusty_core::flume::{bounded as bounded_ch, unbounded as unbounded_ch, Receiver, RecvError, Sender};
 pub use tokio::time::{self, timeout, Duration, Instant};
 pub use tracing::{debug, error, info, trace, warn, Level};
-pub use tracing_tools::{span, PinnedFut, TracingTask};
+pub use tracing_tools::{span, TracingTask};
 pub use url::Url;
 
 pub fn now() -> Duration {
