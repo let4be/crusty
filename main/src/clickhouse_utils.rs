f42ad078e9fd51638507860f3c12120b1cae0704
Sergey F <let4be@gmail.com>
2021-06-27 12:39:55 +0300

    clickhouse writer refactoring

--- main/src/clickhouse_utils.rs
+++ main/src/clickhouse_utils.rs
@@ -25,73 +25,63 @@ impl Writer {
 	) -> Result<()> {
 		let state = Arc::new(Mutex::new(WriterState { notify: Vec::new() }));
 		retry(ExponentialBackoff { max_elapsed_time: None, ..ExponentialBackoff::default() }, || async {
-			Writer::go(self.cfg.clone(), client.clone(), rx.clone(), state.clone())
-				.instrument()
-				.await
-				.map_err(backoff::Error::Transient)?;
-			Ok(())
-		})
-		.await
-	}
+			let cfg = self.cfg.clone();
+			let client = client.clone();
+			let rx = rx.clone();
+			let state = state.clone();
+			TracingTask::new(span!(), async move {
+				let mut inserter = client
+					.inserter(cfg.table_name.as_str())?
+					.with_max_entries(cfg.buffer_capacity as u64)
+					.with_max_duration(*cfg.force_write_duration);
 
-	pub fn go<A: Row + Serialize + Clone + Debug + Send + Sync + 'static>(
-		cfg: ClickhouseWriterConfig,
-		client: Client,
-		rx: Receiver<A>,
-		state: Arc<Mutex<WriterState<A>>>,
-	) -> TracingTask<'static> {
-		TracingTask::new(span!(), async move {
-			let mut inserter = client
-				.inserter(cfg.table_name.as_str())?
-				.with_max_entries(cfg.buffer_capacity as u64)
-				.with_max_duration(*cfg.force_write_duration);
+				let mut last_write = Instant::now();
 
-			let mut last_write = Instant::now();
-
-			let notify = { state.lock().unwrap().notify.clone() };
-			for el in notify.into_iter() {
-				inserter.write(&el).await?;
-			}
-
-			let mut done = false;
-			while !done {
-				let t = Instant::now();
-
-				let s = inserter.commit().await.context("error during inserter.commit")?;
-
-				let since_last = last_write.elapsed();
-				let write_took = t.elapsed();
-
-				if s.entries > 0 {
-					info!(
-						"Clickhouse write to {}/{}({} rows, {} transactions, {}ms since last) finished for {}ms.",
-						&cfg.table_name,
-						&cfg.label,
-						s.entries,
-						s.transactions,
-						since_last.as_millis(),
-						write_took.as_millis()
-					);
-					state.lock().unwrap().notify.clear();
-					last_write = Instant::now();
+				let notify = state.lock().unwrap().notify.clone();
+				for el in notify {
+					inserter.write(&el).await?;
 				}
 
-				if let Ok(r) = timeout(*cfg.check_for_force_write_duration, rx.recv_async()).await {
-					match r {
-						Ok(el) => {
+				loop {
+					let t = Instant::now();
+
+					let s = inserter.commit().await.context("error during inserter.commit")?;
+
+					if s.entries > 0 {
+						let since_last = last_write.elapsed();
+						let write_took = t.elapsed();
+						info!(
+							"Clickhouse write to {}/{}({} rows, {} transactions, {}ms since last) finished for {}ms.",
+							&cfg.table_name,
+							&cfg.label,
+							s.entries,
+							s.transactions,
+							since_last.as_millis(),
+							write_took.as_millis()
+						);
+						state.lock().unwrap().notify.clear();
+						last_write = Instant::now();
+					}
+
+					if let Ok(r) = timeout(*cfg.check_for_force_write_duration, rx.recv_async()).await {
+						if let Ok(el) = r {
 							state.lock().unwrap().notify.push(el.clone());
 							inserter.write(&el).await.context("error during inserter.write")?;
-						}
-						_ => {
-							done = true;
+						} else {
+							break
 						}
 					}
 				}
-			}
 
-			inserter.end().await?;
+				inserter.end().await?;
 
+				Ok(())
+			})
+			.instrument()
+			.await
+			.map_err(backoff::Error::Transient)?;
 			Ok(())
 		})
+		.await
 	}
 }
